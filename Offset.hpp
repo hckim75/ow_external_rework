0fc64622dc5c73999281c51cace325146803b98b
hckim75 <in17854@naver.com>
2026-05-27 13:01:44 +0900

    Create Offset.hpp

---
+++ Offset.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include <cstdint>
+
+namespace offset {
+    struct ScanOffsets {
+        uint64_t Address_entity_base; // 기존 offset::Address_entity_base 값
+        uint32_t Link_UniqueId  = 0xD4;
+        uint32_t Common_UniqueId = 0x138;
+        uint32_t Component_Ptr  = 0x30;
+        uint32_t Entity_Id      = 0x10;
+    };
+}
