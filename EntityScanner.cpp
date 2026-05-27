0666fc293105701fad2e340e0c44dbac9e56d837
hckim75 <in17854@naver.com>
2026-05-27 12:35:35 +0900

    Create EntityScanner.cpp

---
+++ EntityScanner.cpp
@@ -0,0 +1,17 @@
+// Core/EntityScanner.cpp
+#include "EntityScanner.hpp"
+#include "Globals.hpp"
+// TODO: Config.hpp, SDK.hpp 등 필요한 헤더 포함
+
+namespace OW {
+    int abletotread = 0;
+    int entitytime = 0;
+    
+    void entity_scan_thread() {
+        // 기존 entity_scan_thread 내부 코드 복사
+    }
+
+    void entity_thread() {
+        // 기존 entity_thread 내부 코드 (1000줄 가량) 복사
+    }
+}
