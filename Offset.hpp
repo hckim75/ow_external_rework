e3b170b54f61d78fc7343a429d320a9929639044
hckim75 <in17854@naver.com>
2026-05-29 06:46:43 +0900

    Update Offset.hpp

--- Offset.hpp
+++ Offset.hpp
@@ -3,7 +3,7 @@
 
 namespace offset {
     struct ScanOffsets {
-        uint64_t Address_entity_base; // 기존 offset::Address_entity_base 값
+        uint64_t entity_base; // 기존 offset::Address_entity_base 값
         uint32_t Link_UniqueId  = 0xD4;
         uint32_t Common_UniqueId = 0x138;
         uint32_t Component_Ptr  = 0x30;
